998166330a6f4dce8ba743d55add2522195b0e7c
Vishanth <81638898+Vishanth21@users.noreply.github.com>
2025-07-28 22:52:43 +0530

    feat: Implement AC,CE,equals button logic
    -CE needs some testing with exponents
    -Implemented self-repeating operation to equals

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let operand1, operand2, operator;
+let firstOperand = null, displayValue = null, operator = null,waitingSecondOperand = false, tempOperand = null;
 
 function add(a,b) {
     return a+b;
@@ -17,6 +17,8 @@ function divide(a,b) {
 }
 
 function operate(operand1,operand2,operator) {
+    operand1 = Number(operand1);
+    operand2 = Number(operand2);
     switch(operator) {
         case 'add':
             return add(operand1,operand2);
@@ -28,4 +30,41 @@ function operate(operand1,operand2,operator) {
             return divide(operand1,operand2);
 
     };
+}
+
+const display = document.querySelector('.display');
+const buttons = document.querySelector('.buttons');
+buttons.addEventListener('click',function(event) {
+    if(event.target.classList.contains('button')) {
+        if(event.target.id === 'AC') {
+            firstOperand = null;
+            displayValue = null;
+            operator = null;
+            tempOperand = null;
+            waitingSecondOperand = false;
+            display.value = displayValue;
+        }
+        if(event.target.id === 'CE' && display.value) {
+            if(displayValue)
+                displayValue = displayValue.slice(0,displayValue.length - 1);
+            display.value = display.value.slice(0,display.value.length - 1);
+        }
+        if(event.target.id === 'equals') {
+            if(displayValue && firstOperand && operator) {
+                tempOperand = displayValue;
+                printResult(firstOperand,displayValue);                
+            }
+            else if(displayValue === null && firstOperand && operator) {
+                if(!tempOperand)
+                    tempOperand = firstOperand;
+                printResult(firstOperand,tempOperand);
+            }
+        }
+    }    
+})
+
+function printResult(first,second) {
+    display.value = operate(first,second,operator);
+    firstOperand = display.value;
+    displayValue = null;
 }
