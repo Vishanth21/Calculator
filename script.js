3c86f5a6d78fd6c8ce5737322d772f321e338963
Vishanth <81638898+Vishanth21@users.noreply.github.com>
2025-07-31 14:40:41 +0530

    refactor: Encapsulate states,functions into object

--- script.js
+++ script.js
@@ -1,36 +1,37 @@
-let firstOperand = null, displayValue = null, operator = null,waitingSecondOperand = false, tempOperand = null;
-
-function add(a,b) {
-    return a+b;
-}
-
-function subtract(a,b) {
-    return a-b;
-}
-
-function multiply(a,b) {
-    return a*b;
-}
-
-function divide(a,b) {
-    return a/b;
-}
-
-function operate(operand1,operand2,operator) {
-    operand1 = Number(operand1);
-    operand2 = Number(operand2);
-    switch(operator) {
-        case 'add':
-            return add(operand1,operand2);
-        case 'subtract':
-            return subtract(operand1,operand2);
-        case 'multiply':
-            return multiply(operand1,operand2);
-        case 'divide':
-            if(operand2 === 0)
-                return "so tuff 🥀";
-            return divide(operand1,operand2);
-    };
+const calculator = {
+    firstOperand: null,
+    displayValue: null,
+    operator: null,
+    waitingSecondOperand: false,
+    tempOperand: null,
+    add(a,b) {
+        return a+b;
+    },
+    subtract(a,b) {
+        return a-b;
+    },
+    multiply(a,b) {
+        return a*b;
+    },
+    divide(a,b) {
+        return a/b;
+    },  
+    operate(operand1,operand2,operator) {
+        operand1 = Number(operand1);
+        operand2 = Number(operand2);
+        switch(operator) {
+            case 'add':
+                return this.add(operand1,operand2);
+            case 'subtract':
+                return this.subtract(operand1,operand2);
+            case 'multiply':
+                return this.multiply(operand1,operand2);
+            case 'divide':
+                if(operand2 === 0)
+                    return "so tuff 🥀";
+                return this.divide(operand1,operand2);
+        }
+    },
 }
 
 const display = document.querySelector('.display');
@@ -46,87 +47,88 @@ window.addEventListener('DOMContentLoaded',clearAll);
 
 buttons.addEventListener('click',function(event) {
     if(event.target.classList.contains('button')) {
+        console.log(event.target.id);
         if(event.target.id === 'AC') {
             clearAll();
         }
         if(event.target.id === 'CE' && display.value) {
-            if(displayValue)
-                displayValue = displayValue.slice(0,displayValue.length - 1);
-            display.value = display.value.slice(0,display.value.length - 1);
+            if(calculator.displayValue)
+                calculator.displayValue = calculator.displayValue.slice(0,calculator.displayValue.length - 1);
+            display.value = calculator.displayValue;
         }
         if(event.target.id === 'equals') {
             removeActiveOperator();
-            if(displayValue && firstOperand && operator) {
-                tempOperand = displayValue;
-                printResult(firstOperand,displayValue);                
+            if(calculator.displayValue && calculator.firstOperand && calculator.operator) {
+                calculator.tempOperand = calculator.displayValue;
+                printResult(calculator.firstOperand,calculator.displayValue);                
             }
-            else if(displayValue === null && firstOperand && operator) {
-                if(!tempOperand)
-                    tempOperand = firstOperand;
-                printResult(firstOperand,tempOperand);
+            else if(calculator.displayValue === null && calculator.firstOperand && calculator.operator) {
+                if(!calculator.tempOperand)
+                    calculator.tempOperand = calculator.firstOperand;
+                printResult(calculator.firstOperand,calculator.tempOperand);
             }
         }
         if(event.target.id === 'percentage') {
-            if(displayValue) {
-                displayValue = (Number(displayValue)/100).toString();
-                display.value = displayValue;
+            if(calculator.displayValue) {
+                calculator.displayValue = (Number(calculator.displayValue)/100).toString();
+                display.value = calculator.displayValue;
             }
         }
         if(event.target.classList.contains('operator') && event.target.id !== 'percentage') {
             removeActiveOperator();
             event.target.classList.add('active');
-            if(firstOperand === null) {
-                firstOperand = displayValue;
-                displayValue = null;
+            if(calculator.firstOperand === null) {
+                calculator.firstOperand = calculator.displayValue;
+                calculator.displayValue = null;
                 display.value = '';
-                operator = event.target.id;
-                waitingSecondOperand = true;
+                calculator.operator = event.target.id;
+                calculator.waitingSecondOperand = true;
             }
-            else if(waitingSecondOperand) {
-                printResult(firstOperand,displayValue);
-                operator = event.target.id;
+            else if(calculator.waitingSecondOperand) {
+                printResult(calculator.firstOperand,calculator.displayValue);
+                calculator.operator = event.target.id;
             }
         }
         if(event.target.classList.contains('digit') || event.target.id === 'decimal') {
             if(event.target.id === 'decimal') {
-                if(!displayValue) {
-                    displayValue = '0.';
-                    display.value = displayValue;
+                if(!calculator.displayValue) {
+                    calculator.displayValue = '0.';
+                    display.value = calculator.displayValue;
                 }
-                else if(!displayValue.includes('.')) {
-                    displayValue += '.';
-                    display.value = displayValue;
+                else if(!calculator.displayValue.includes('.')) {
+                    calculator.displayValue += '.';
+                    display.value = calculator.displayValue;
                 }
             }
             else {
-                if(!displayValue)
-                    displayValue = '';
-                displayValue += event.target.id;
-                display.value = displayValue;
+                if(!calculator.displayValue)
+                    calculator.displayValue = '';
+                calculator.displayValue += event.target.id;
+                display.value = calculator.displayValue;
             }
         }
     }    
 })
 
 function printResult(first,second) {
-    const result = operate(first,second,operator);
+    const result = calculator.operate(first,second,calculator.operator);
     if(typeof result === 'string') {
         display.value = result;
         setTimeout(() => clearAll(),1500);
     }
     else {
         display.value = result;
-        firstOperand = result;
-        displayValue = null;
+        calculator.firstOperand = result;
+        calculator.displayValue = null;
     }
 }
 
 function clearAll() {
-    firstOperand = null;
-    displayValue = null;
-    operator = null;
-    tempOperand = null;
-    waitingSecondOperand = false;
+    calculator.firstOperand = null;
+    calculator.displayValue = null;
+    calculator.operator = null;
+    calculator.tempOperand = null;
+    calculator.waitingSecondOperand = false;
     display.value = '';
     removeActiveOperator();
 }
